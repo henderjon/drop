725e876f7969385b0504b8057b462ecaaa32ca71
henderjon <jhenderson@capstonedigital.com>
2014-04-10 22:40:54 -0500

    removes safety feature

--- src/drop.php
+++ src/drop.php
@@ -5,40 +5,39 @@
  * @author @henderjon / @donatj
  * @return
  */
-if( !function_exists("drop") ){
-	function drop(){
 
-		$stream = "";
+function drop(){
 
-		foreach(func_get_args() as $key => $arg){
+	$stream = "";
 
-			$stream .= "\n\n";
-			$stream .= "/// Arg No {$key}\n";
-			$stream .= str_repeat("/", 72) . "\n\n";
+	foreach(func_get_args() as $key => $arg){
 
-			switch(true){
-				case is_null($arg) :
-					$stream .= print_r("(null)null", true);
-				break;
-				case false === $arg :
-					$stream .= print_r("(bool)false", true);
-				break;
-				case true === $arg :
-					$stream .= print_r("(bool)true", true);
-				break;
-				default:
-					$stream .= print_r($arg, true);
-				break;
-			}
+		$stream .= "\n\n";
+		$stream .= "/// Arg No {$key}\n";
+		$stream .= str_repeat("/", 72) . "\n\n";
 
-			$stream .= "\n\n";
+		switch(true){
+			case is_null($arg) :
+				$stream .= print_r("(null)null", true);
+			break;
+			case false === $arg :
+				$stream .= print_r("(bool)false", true);
+			break;
+			case true === $arg :
+				$stream .= print_r("(bool)true", true);
+			break;
+			default:
+				$stream .= print_r($arg, true);
+			break;
 		}
 
-		if(0 === stripos(php_sapi_name(), "cli")){
-			fwrite(STDOUT, $stream);
-		}else{
-			echo "<pre>{$stream}</pre>";
-		}
-		exit;
+		$stream .= "\n\n";
 	}
+
+	if(0 === stripos(php_sapi_name(), "cli")){
+		fwrite(STDOUT, $stream);
+	}else{
+		echo "<pre>{$stream}</pre>";
+	}
+	exit;
 }
